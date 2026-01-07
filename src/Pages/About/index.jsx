d4368a257df8824f89163aa7479ed37eff738a84
hasnain mubarak <hasnainmubarak075@gmail.com>
2026-01-07 13:23:10 +0500

    more changes

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -4,7 +4,6 @@ const About = () => {
   return (
     <div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
-    
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,9 +1,22 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react'
 
 const Contact = () => {
+  const classes = useStyles();
   return (
-    <div>Contact</div>
+     <div className={classes.contain}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p></div>
   )
 }
 
 export default Contact
+const useStyles = makeStyles((theme) => ({
+  contain: {
+    padding:"20px",
+  },
+}));
