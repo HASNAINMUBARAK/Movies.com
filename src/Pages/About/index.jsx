3722fce3c8658cecf17eeceb71e79843e249b7cf
hasnain mubarak <hasnainmubarak075@gmail.com>
2026-01-07 13:17:36 +0500

    UI change

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -8,6 +8,7 @@ const About = () => {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora dignissimos cumque consequuntur veniam maxime voluptas reprehenderit blanditiis! Perferendis doloribus deleniti excepturi libero eaque fugiat porro a dolorem laudantium?</p></div>
   )
 }
